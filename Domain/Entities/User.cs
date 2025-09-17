df57e0227bde2dc78b19d6029fc0acb0866ac9af
saidmahmud7 <boboevs7777@gmail.com>
2025-09-17 13:29:11 +0300

    feature:fix a lot of bags

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,6 +6,6 @@ public class User : IdentityUser
 {
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public Employee? Employee { get; set; }
-    public string RoleId { get; set; }
+    public string? RoleId { get; set; }
     public IdentityRole Role { get; set; }
 }
---
+++ Infrastructure/Migrations/20250917102628_Updatefield.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatefield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_RoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_RoleId",
+                table: "AspNetUsers",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_RoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AspNetRoles_RoleId",
+                table: "AspNetUsers",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
